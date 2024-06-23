76fe686be9048f5418fd41ef6aa335e32ce9b997
Дмитрий Тульников <tulnikov@MacBookPro.local>
2024-06-24 00:18:16 +0300

    Почти конец первой часты
    Пересмотреть урок 24

---
+++ src/components/CostDay.js
@@ -0,0 +1,17 @@
+import './CostDay.css'
+
+function CostDay(props) {
+    const month = props.date.toLocaleString('ru-RU', {month: "long"})
+    const year = props.date.getFullYear()
+    const day = props.date.toLocaleString('ru-RU', {day: "2-digit"})
+
+    return (
+        <div className="cost-day">
+            <div className='cost-day__month'>{month}</div>
+            <div className='cost-day__year'>{year}</div>
+            <div className='cost-day__day'>{day}</div>
+        </div>
+    )
+}
+
+export default CostDay;
